f4a4623b1c79fa599695deadb1700f3495cdd1e5
yp <spvirus@126.com>
2015-03-12 22:26:43 +0800

    123

--- SurveyDesign/JSDW/ApplySGXKZGL/EmpInfo.aspx.cs
+++ SurveyDesign/JSDW/ApplySGXKZGL/EmpInfo.aspx.cs
@@ -87,66 +87,60 @@ public partial class JSDW_APPLYSGXKZGL_EmpInfo : System.Web.UI.Page
     //保存
     private void saveInfo()
     {
-
-        string sql = @" select count(*) from TC_PrjItem_Emp
-                            where EmpType='11220201'
-                            and FAppId='{0}'";
-        sql = string.Format(sql, t_FAppId.Value);
-        int count = SConvert.ToInt(dbContext.ExecuteQuery<int>(sql).FirstOrDefault());
-        dbContext = new EgovaDB();
-        string sql1 = @" select count(*) from TC_PrjItem_Emp
-                            where FIdCard='{0}'
-                            and FAppId='{1}'";
-        sql1 = string.Format(sql1, t_FIdCard.Text, t_FAppId.Value);
+        string sql1 = @" select count(*) from TC_PrjItem_Emp  where FIdCard='{0}'";
+        sql1 = string.Format(sql1, t_FIdCard.Text);
         int count1 = SConvert.ToInt(dbContext.ExecuteQuery<int>(sql1).FirstOrDefault());
-        dbContext = new EgovaDB();
-        string sql2 = @" select count(*) from TC_PrjItem_Emp
-                            where EmpType='11220209'
-                            and FAppId='{0}'";
-        sql2 = string.Format(sql2, t_FAppId.Value);
-        int count2 = SConvert.ToInt(dbContext.ExecuteQuery<int>(sql2).FirstOrDefault());
-        if (t_EmpType.SelectedValue == "11220201" && count > 0)
-        {
-            ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('项目负责人只能添加一位');window.returnValue='1';", true);
-        }
-        else if (count1 > 0)
+        if (count1 > 0)
         {
             ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('人员不允许重复添加');window.returnValue='1';", true);
+            return;
         }
-        else if (count2 > 0)
+
+        string sql = @" select count(*) from TC_PrjItem_Emp  where EmpType='{0}' and FAppId='{1}'";
+        sql = string.Format(sql, t_EmpType.SelectedValue, t_FAppId.Value);
+        int count = SConvert.ToInt(dbContext.ExecuteQuery<int>(sql).FirstOrDefault());
+        if (count > 0)
         {
-            ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('总监理工程师只能添加一位');window.returnValue='1';", true);
+            switch (t_EmpType.SelectedValue)
+            {
+                //项目负责人
+                case "11220201":
+                    ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('项目负责人只能添加一位');window.returnValue='1';", true);
+                    return;
+                //总监理工程师
+                case "11220209":
+                    ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('总监理工程师只能添加一位');window.returnValue='1';", true);
+                    return;
+            }
+        }
+
+        dbContext = new EgovaDB();
+        string fId = txtFId.Value;
+        TC_PrjItem_Emp Emp = new TC_PrjItem_Emp();
+        if (!string.IsNullOrEmpty(fId))
+        {
+            Emp = dbContext.TC_PrjItem_Emp.Where(t => t.FId == fId).FirstOrDefault();
         }
         else
         {
-            dbContext = new EgovaDB();
-            string fId = txtFId.Value;
-            TC_PrjItem_Emp Emp = new TC_PrjItem_Emp();
-            if (!string.IsNullOrEmpty(fId))
-            {
-                Emp = dbContext.TC_PrjItem_Emp.Where(t => t.FId == fId).FirstOrDefault();
-            }
-            else
-            {
-                fId = Guid.NewGuid().ToString();
-                Emp.FId = fId;
-                Emp.FEmpId = h_selEmpId.Value;
-                Emp.FPrjItemId = t_FPrjItemId.Value;
-                Emp.FAppId = t_FAppId.Value;
-                Emp.FTime = DateTime.Now;
-                Emp.FCreateTime = DateTime.Now;
-                Emp.FEntId = t_FEntId.Value;
-                dbContext.TC_PrjItem_Emp.InsertOnSubmit(Emp);
-            }
-            pageTool tool = new pageTool(this.Page);
-            Emp = tool.getPageValue(Emp);
-            dbContext.SubmitChanges();
-            txtFId.Value = fId;
-            ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('保存成功');window.returnValue='1';", true);
+            fId = Guid.NewGuid().ToString();
+            Emp.FId = fId;
+            Emp.FEmpId = h_selEmpId.Value;
+            Emp.FPrjItemId = t_FPrjItemId.Value;
+            Emp.FAppId = t_FAppId.Value;
+            Emp.FTime = DateTime.Now;
+            Emp.FCreateTime = DateTime.Now;
+            Emp.FEntId = t_FEntId.Value;
+            dbContext.TC_PrjItem_Emp.InsertOnSubmit(Emp);
         }
+        pageTool tool = new pageTool(this.Page);
+        Emp = tool.getPageValue(Emp);
+        dbContext.SubmitChanges();
+        txtFId.Value = fId;
+        ScriptManager.RegisterClientScriptBlock(up_Main, typeof(UpdatePanel), "js", "alert('保存成功');window.returnValue='1';", true);
 
-        //     MyPageTool.showMessageAjax("保存成功ii", up_Main);
-        //    MyPageTool.showMessageAndRunFunctionAjax("保存成功", "window.returnValue='1';", up_Main);
+
+
     }
     public void Alert(string str_Message)
     {
