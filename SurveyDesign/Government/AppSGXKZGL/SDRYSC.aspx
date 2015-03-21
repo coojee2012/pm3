ad5b0978e35a4dd4edee7896fce416649c66038e
coojee2012 <11366846@qq.com>
2015-03-21 16:09:07 +0800

    加入锁定人员的源文件 未完成： 1、办理查询： 需要确定“备案号”字段来源， 需要新增详情页面，按需求比较复杂 2、《四川省建筑市场和工程质量安全监管一体化工作平台-平台修改意见》部分功能

---
+++ SurveyDesign/Government/AppSGXKZGL/SDRYSC.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SDRYSC.aspx.cs" Inherits="Government_AppSGXKZGL_SDRYSC" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>锁定人员核查</title>
+     <script type="text/javascript" src="../../script/jquery.js"></script>
+
+    <script type="text/javascript" src="../../script/default.js"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+   <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <input type="hidden"  runat="server" ID="h_FAppId" value="" />
+        <input type="hidden"  runat="server" ID="h_FPrjItemId" value="" />
+                     <asp:DataGrid ID="JustAppInfo_List" runat="server" AutoGenerateColumns="False" CssClass="m_dg1"
+                        HorizontalAlign="Center" OnItemDataBound="JustAppInfo_List_ItemDataBound" Width="98%">
+                        <HeaderStyle CssClass="m_dg1_h" />
+                        <ItemStyle CssClass="m_dg1_i" />
+                        <Columns>
+                             <asp:BoundColumn HeaderText="姓名" HeaderStyle-HorizontalAlign="Center" HeaderStyle-Width="100px" DataField="FHumanName">
+                <ItemStyle Wrap="False" HorizontalAlign="Center" CssClass="padLeft" Width="100px" />
+                <HeaderStyle Font-Underline="False" Wrap="False" />
+            </asp:BoundColumn>
+                             <asp:BoundColumn HeaderText="锁定次数" HeaderStyle-HorizontalAlign="Center" HeaderStyle-Width="100px" DataField="FCount">
+                <ItemStyle Wrap="False" HorizontalAlign="Center" CssClass="padLeft" Width="100px" />
+                <HeaderStyle Font-Underline="False" Wrap="False" />
+            </asp:BoundColumn>
+                            <asp:BoundColumn HeaderText="锁定地区" HeaderStyle-HorizontalAlign="Center" DataField="FAddress">
+                <ItemStyle Wrap="False" HorizontalAlign="Center" CssClass="padLeft" />
+                <HeaderStyle Font-Underline="False" Wrap="False" />
+            </asp:BoundColumn>
+                            </Columns>
+                         </asp:DataGrid>
+
+     
+    </form>
+</body>
+</html>
---
+++ SurveyDesign/Government/AppSGXKZGL/SDRYSC.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using EgovaDAO;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Government_AppSGXKZGL_SDRYSC : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            //base.Page_Load(sender, e);
+            h_FAppId.Value = EConvert.ToString(Request["FAppId"]);
+            h_FPrjItemId.Value = EConvert.ToString(Request["FPrjItemId"]);
+           
+            ShowInfo();
+        }
+
+    }
+    private void ShowInfo()
+    {
+        using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["dbCenter"].ConnectionString))
+        {
+            string sql = "SELECT  A.FIdCard,A.FHumanName,A.IsLock,SUM(A.SelectedCount) AS FCount,FAddress=STUFF((select ','+ISNULL([Address],'--') from TC_PrjItem_Info  where fid=A.FPrjItemId for xml path('')),1,1,'') ";
+            sql += " FROM TC_PrjItem_Emp_Lock A ";
+            sql += " WHERE A.FIdCard IN (SELECT  B.FIdCard FROM  TC_PrjItem_Emp AS B WHERE  B.FAppId='" + h_FAppId.Value + "')";
+            sql += " Group BY A.FIdCard,A.FHumanName,A.IsLock,A.FPrjItemId ";
+
+            sql += " union all SELECT  b.FIdCard, B.FHumanName,0 as IsLock ,0 As FCount,(SELECT Address from  TC_PrjItem_Info  where fid=B.FPrjItemId ) as Address";
+            sql += " FROM  TC_PrjItem_Emp B WHERE  B.FAppId='" + h_FAppId.Value + "' AND B.FIdCard NOT IN ";
+ sql += " (SELECT FIdCard FROM  TC_PrjItem_Emp_Lock )";
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(ds, "ds");
+            DataTable dt = ds.Tables[0];
+            JustAppInfo_List.DataSource = dt;
+            JustAppInfo_List.DataBind();
+           // SqlCommand cmd = new SqlCommand(sql, conn);
+            //conn.Open();
+            //int a = cmd.ExecuteNonQuery();
+        }
+       
+        //this.Pager1.sql = sql ;
+        //this.Pager1.controltype = "DataGrid";
+        //this.Pager1.controltopage = "JustAppInfo_List";
+        //this.Pager1.pagecount = 15;
+        //this.Pager1.dataBind();
+    }
+    protected void JustAppInfo_List_ItemDataBound(object sender, DataGridItemEventArgs e)
+    {
+        if (e.Item.ItemIndex > -1)
+        { 
+        
+        }
+    }
+}
