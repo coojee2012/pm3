eb91d2190500808478c019ad1e7fb52c9f0b5968
coojee2012 <11366846@qq.com>
2015-04-12 22:18:55 +0800

    开竣工及停复工相关表修改及新增内容； 另停复工中锁定人员管理

---
+++ SurveyDesign/Government/AppTFGGL/JCSD.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="JCSD.aspx.cs" Inherits="Government_AppTFGGL_JCSD" %>
+
+<%@ Register Src="../../common/govdeptid2.ascx" TagName="govdeptid" TagPrefix="uc1" %>
+<%@ Register Src="../../Common/pager.ascx" TagName="pager" TagPrefix="uc1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>项目人员列表</title>
+    <asp:Link id="skin1" runat="server">
+    </asp:Link>
+
+    <script type="text/javascript" src="../../script/jquery.js"></script>
+
+    <script type="text/javascript" src="../../script/default.js"></script>
+
+    <script type="text/javascript" src="../../DateSelect/WdatePicker.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            txtCss();
+        });
+        function CheckInfo() {
+            return AutoCheckInfo();
+        }
+    </script>
+
+    <base target="_self"></base>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <input type="hidden" runat="server" id="t_FIdCard" value="" />
+         <table width="98%" align="center" class="m_title">
+            <tr>
+                <th >解锁原因:
+                </th>
+               
+                     <td >
+                    <asp:TextBox ID="t_JSYY" runat="server" CssClass="m_txt" Width="400px"
+                        MaxLength="30"></asp:TextBox>
+                </td>
+                <td>
+                     <asp:Button ID="btnJS" runat="server" Text="解锁" CssClass="m_btn_w2" OnClick="btnJS_Click"   />
+                     <asp:Button ID="btnReload" runat="server" Text="刷新" CssClass="m_btn_w2" OnClick="btnReload_Click"   />
+                </td>
+            </tr>
+             </table>
+        <asp:DataGrid ID="dg_List" runat="server" AutoGenerateColumns="False" CssClass="m_dg1"
+            HorizontalAlign="Center" OnItemDataBound="App_List_ItemDataBound" Style="margin-top: 6px; margin-bottom: 1px;"
+            Width="98%" OnItemCommand="dg_List_ItemCommand">
+            <HeaderStyle CssClass="m_dg1_h" />
+            <ItemStyle CssClass="m_dg1_i" />
+            <Columns>
+              <asp:TemplateColumn>
+                            <ItemStyle Width="20px" />
+                            <HeaderTemplate>
+                                <asp:CheckBox ID="checkAll" runat="server" onclick="checkAll(this);" />
+                            </HeaderTemplate>
+                            <ItemTemplate>
+                                <asp:CheckBox ID="CheckItem" runat="server" />
+                            </ItemTemplate>
+                        </asp:TemplateColumn>
+                <asp:BoundColumn HeaderText="序号">
+                    <ItemStyle Font-Underline="False" Width="30px" Wrap="False" />
+                    <HeaderStyle Font-Underline="False" Wrap="False" />
+                </asp:BoundColumn>
+                <asp:BoundColumn DataField="FHumanName" HeaderText="姓名"></asp:BoundColumn>
+                <asp:BoundColumn DataField="PrjItemName" HeaderText="相关项目"></asp:BoundColumn>
+     
+                <asp:BoundColumn DataField="lockType" HeaderText="锁定类型"></asp:BoundColumn>
+                <asp:BoundColumn DataField="FCreateTime" HeaderText="锁定时间" DataFormatString="yyyy-MM-dd"></asp:BoundColumn>
+                <asp:BoundColumn DataField="FTime" HeaderText="锁定结束时间" DataFormatString="yyyy-MM-dd"></asp:BoundColumn>          
+                <asp:BoundColumn DataField="FId" Visible="False"></asp:BoundColumn>
+            </Columns>
+        </asp:DataGrid>
+        <div class="d div1 tcen" style="width: 98%; margin: 0px auto;">
+        <uc1:pager ID="Pager1" runat="server"></uc1:pager>
+    </div>
+    </form>
+</body>
+</html>
--- SurveyDesign/Government/AppTFGGL/RWSDList.aspx.cs
+++ SurveyDesign/Government/AppTFGGL/RWSDList.aspx.cs
@@ -11,7 +11,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Government_AppTFGGL_FGList : govBasePage
+public partial class Government_AppTFGGL_RWSDList : govBasePage
 {
     RCenter rc = new RCenter();
     SaveAsBase sab = new SaveAsBase();
@@ -32,12 +32,12 @@ public partial class Government_AppTFGGL_FGList : govBasePage
     {
         StringBuilder sb = new StringBuilder();
         sb.Append("select * from ( ");
-        sb.Append(" select qa.*,ep.FID as FepId,a.FId as FFId,a.FYJSJFGRQ, case a.FCLZT when 0 then '待处理' else '通过' end  as FCLZT ,");
-        sb.Append(" case  when a.FYJSJFGRQ <= GETDATE() then '已复工' else '已停工' end as FSGZT  ");
-        sb.Append(" from TC_SGXKZ_TFG a ");
-        sb.Append(" left join TC_SGXKZ_PrjInfo qa on a.FAppId = qa.FAppId ");
-        sb.Append(" left join CF_App_ProcessInstanceBackup ep on ep.FLinkId = qa.FAppId ");
-        sb.Append(" where a.FType = 1");
+        sb.Append(" select distinct a.FId,a.FAppId,a.IsLock,a.SelectedCount,case b.FSex when 1 then '男' else '女' end as FSex,");
+        sb.Append(" b.XMZW,b.FIdCard,b.FHumanName,b.FEntName,b.ZSBH,c.PrjItemName   ");
+        sb.Append(" from TC_PrjItem_Emp_Lock a ");
+        sb.Append(" left join TC_PrjItem_Emp b on a.FIdCard=b.FIdCard ");
+        sb.Append(" left join TC_SGXKZ_PrjInfo c on c.FAppId = a.FAppId ");
+        sb.Append(" where a.IsLock = 1 ");
         sb.Append(getCondi());
         sb.Append(" ) as ttt where 1=1");
 
@@ -55,17 +55,25 @@ public partial class Government_AppTFGGL_FGList : govBasePage
     private string getCondi()
     {
         StringBuilder sb = new StringBuilder();
-        if (this.txtFPrjItemName.Text.Trim() != "" && this.txtFPrjItemName.Text.Trim() != null)
+        if (this.txtXM.Text.Trim() != "" && this.txtXM.Text.Trim() != null)
         {
-            sb.Append(" and qa.PrjItemName like '%" + this.txtFPrjItemName.Text.Trim() + "%' ");
+            sb.Append(" and b.FHumanName like '%" + this.txtXM.Text.Trim() + "%' ");
+        }
+        if (this.txtIDCard.Text.Trim() != "" && this.txtIDCard.Text.Trim() != null)
+        {
+            sb.Append(" and b.FIdCard like '%" + this.txtIDCard.Text.Trim() + "%' ");
         }
         if (this.txtSGXKZBH.Text.Trim() != "" && this.txtSGXKZBH.Text.Trim() != null)
         {
-            sb.Append(" and qa.SGXKZBH like '%" + this.txtSGXKZBH.Text.Trim() + "%' ");
+            sb.Append(" and b.ZSBH like '%" + this.txtSGXKZBH.Text.Trim() + "%' ");
         }
-        if (this.txtBLZT.SelectedValue != null && this.txtBLZT.SelectedValue != "-1")
+        if (this.txtQYMC.Text.Trim() != "" && this.txtQYMC.Text.Trim() != null)
         {
-            sb.Append(" and a.FCLZT =  " + this.txtBLZT.SelectedValue);
+            sb.Append(" and b.FEntName like '%" + this.txtQYMC.Text.Trim() + "%' ");
+        }
+        if (this.txtFBZT.SelectedValue != null && this.txtFBZT.SelectedValue != "-1")
+        {
+            //sb.Append(" and b.ZSBH =  " + this.txtSGXKZBH.SelectedValue);
         }
 
 
@@ -87,7 +95,8 @@ public partial class Government_AppTFGGL_FGList : govBasePage
         if (e.Item.ItemIndex > -1)
         {
             string fAppId = EConvert.ToString(DataBinder.Eval(e.Item.DataItem, "FAppId"));
-            string fId = EConvert.ToString(DataBinder.Eval(e.Item.DataItem, "FFId"));
+            string fId = EConvert.ToString(DataBinder.Eval(e.Item.DataItem, "Fid"));
+            string idCard = EConvert.ToString(DataBinder.Eval(e.Item.DataItem, "FIdCard"));
 
             CheckBox box = (CheckBox)e.Item.Cells[0].Controls[1];
             box.Attributes["id"] = "span" + box.ClientID;
@@ -96,8 +105,8 @@ public partial class Government_AppTFGGL_FGList : govBasePage
             box.Attributes["name"] = fAppId;
 
             e.Item.Cells[1].Text = ((e.Item.ItemIndex + 1) + this.Pager1.pagecount * (this.Pager1.curpage - 1)).ToString();
-            e.Item.Cells[2].Text = "<a href='javascript:void(0)' onclick=\"showAddWindow('GCXX.aspx?FId=" + fId + "&FAppId=" + fAppId + "',900,600);\">" + e.Item.Cells[2].Text + "</a>";
-            e.Item.Cells[3].Text = "<a href='javascript:void(0)' onclick=\"showAddWindow('SGXKZXX.aspx?FId=" + fId + "&FAppId=" + fAppId + "',900,600);\">" + e.Item.Cells[3].Text + "</a>";
+            e.Item.Cells[7].Text = "<a href='javascript:void(0)' onclick=\"showAddWindow('JCSD.aspx?idCard=" + idCard + "&FAppId=" + fAppId + "',900,600);\">" + e.Item.Cells[7].Text + "</a>";
+          //  e.Item.Cells[3].Text = "<a href='javascript:void(0)' onclick=\"showAddWindow('SGXKZXX.aspx?FId=" + fId + "&FAppId=" + fAppId + "',900,600);\">" + e.Item.Cells[3].Text + "</a>";
 
         }
     }
