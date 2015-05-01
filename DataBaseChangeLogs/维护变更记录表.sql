215a7a2ae98b86d207029fdecfe0be8485583c14
cdrooom@foxmail.com <cdrooom@foxmail.com>
2015-05-01 11:51:27 +0800

    维护变更记录

---
+++ DataBaseChangeLogs/维护变更记录表.sql
@@ -0,0 +1,75 @@
+if exists (select 1
+          from sysobjects
+          where id = object_id('dbo.Tri_del_RepPrjinfo')
+          and type = 'TR')
+   drop trigger dbo.Tri_del_RepPrjinfo
+go
+
+
+create trigger dbo.Tri_del_RepPrjinfo on dbo.TC_SGXKZ_BGJG for insert as
+begin
+    delete TC_SGXKZ_BGJG
+      from TC_SGXKZ_BGJG a,inserted b 
+     where a.FAppid     = b.FAppid
+       and a.FPrjItemId = b.FPrjItemId
+       and a.BGNR       = a.BGNR
+
+    return
+end
+go
+
+
+--删除重复的企业信息
+if exists (select 1
+          from sysobjects
+          where id = object_id('dbo.Tri_Del_RepQyinfo')
+          and type = 'TR')
+   drop trigger dbo.Tri_Del_RepQyinfo
+go
+
+
+create trigger dbo.Tri_Del_RepQyinfo on dbo.TC_SGXKZ_QYBGJG 
+after insert as
+begin
+    delete TC_SGXKZ_QYBGJG
+      from TC_SGXKZ_QYBGJG c
+	 where exists(select 1 from (select a.FLinkId,a.FAppid,a.FPrjItemId from TC_SGXKZ_QYBGJG a, inserted b 
+	                                           where a.FAppid     = b.FAppid
+                                                 and a.FPrjItemId = b.FPrjItemId
+                                                 and a.FLinkId    = b.FLinkId 
+											 group by a.FLinkId,a.FAppid,a.FPrjItemId
+											   having count(1) >1
+							    ) d
+						   where c.FLinkId = d.FLinkId 
+						     and c.FAppid = d.FAppid 
+							 and c.FPrjItemId = d.FPrjItemId)
+end
+go
+
+if exists (select 1
+          from sysobjects
+          where id = object_id('dbo.Tri_Del_RepRyinfo')
+          and type = 'TR')
+   drop trigger dbo.Tri_Del_RepRyinfo
+go
+
+
+--删除重复的人员信息
+create trigger dbo.Tri_Del_RepRyinfo on dbo.TC_SGXKZ_RYBGJG 
+after insert as
+begin
+  delete TC_SGXKZ_RYBGJG
+      from TC_SGXKZ_RYBGJG c
+	 where exists(select 1 from (select a.FLinkId,a.FAppid,a.FPrjItemId from TC_SGXKZ_RYBGJG a, inserted b 
+	                                           where a.FAppid     = b.FAppid
+                                                 and a.FPrjItemId = b.FPrjItemId
+                                                 and a.FLinkId    = b.FLinkId 
+											 group by a.FLinkId,a.FAppid,a.FPrjItemId
+											   having count(1) >1
+							    ) d
+						   where c.FLinkId = d.FLinkId 
+						     and c.FAppid = d.FAppid 
+							 and c.FPrjItemId = d.FPrjItemId)
+end
+go
+
