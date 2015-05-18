ee8a5dea91108d6b766d886578f7113ec0d89452
cdstarsong <scstarsong@qq.com>
2015-05-18 16:51:18 +0800

    只能查询自己管辖范围的施工许可证信息

--- SurveyDesign/Government/AppSGXKZGL/CSList.aspx.cs
+++ SurveyDesign/Government/AppSGXKZGL/CSList.aspx.cs
@@ -115,7 +115,7 @@ public partial class Government_AppSGXKZGL_CSList : govBasePage
         //  sb.Append(" and ep.FSubFlowId = er.FSubFlowId "); //去掉这行，表示可以查询已经处理了到了下一阶段的业务
         sb.Append(" and ep.flinkId = er.FLinkId  and ep.flinkId = qa.FAppId ");
         //sb.Append(" and er.FRoleId in (" + Session["DFRoleId"].ToString() + ")");
-        sb.Append(" and ap.FUpDeptId like '" + Session["DFId"].ToString() + "%' ");
+        sb.Append(" and ap.FUpDeptId = '" + Session["DFId"].ToString() + "' ");
         sb.Append(" and ep.FLinkId = ap.FId ");
         sb.Append(getCondi());
         //下面的查询备份表
@@ -131,7 +131,7 @@ public partial class Government_AppSGXKZGL_CSList : govBasePage
         //  sb.Append(" and ep.FSubFlowId = er.FSubFlowId "); //去掉这行，表示可以查询已经处理了到了下一阶段的业务
         sb.Append(" and ep.flinkId = er.FLinkId  and ep.flinkId = qa.FAppId ");
         //sb.Append(" and er.FRoleId in (" + Session["DFRoleId"].ToString() + ")");
-        sb.Append(" and ap.FUpDeptId like '" + Session["DFId"].ToString() + "%' ");
+        sb.Append(" and ap.FUpDeptId = '" + Session["DFId"].ToString() + "' ");
         sb.Append(" and ep.FLinkId = ap.FId ");
         sb.Append(getCondi());
         sb.AppendLine(" ) ttt where 1=1 ");
